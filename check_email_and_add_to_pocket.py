f2cafcf1ae6ee4b726a4bd8579d424912ae70f4b
muxueqz <zhangmingyuan240@gmail.com>
2024-10-21 17:56:08 +0800

    some imap cannot search header, so i changed it

--- check_email_and_add_to_pocket.py
+++ check_email_and_add_to_pocket.py
@@ -1,5 +1,6 @@
 import imaplib
 import email
+from email.header import decode_header
 import re
 from pocket import Pocket
 import os
@@ -24,7 +25,7 @@ pocket_instance = Pocket(pocket_consumer_key, pocket_access_token)
 
 # Search for unread messages with the specified recipient
 recipient_email = os.environ.get("POCKET_RECIPIENT_EMAIL")
-search_criteria = f'(UNSEEN HEADER To {recipient_email})'
+search_criteria = 'UNSEEN'
 status, messages = imap.search(None, search_criteria)
 messages = messages[0].split(b' ')
 for message in messages:
@@ -38,7 +39,10 @@ for message in messages:
                 message_body = part.get_payload(decode=True).decode()
     else:
         message_body = email_message.get_payload(decode=True).decode()
-
+    to_ = email_message.get("To")
+    print(f"To: {to_}")
+    if f"{recipient_email}" not in to_:
+        continue
     # Extract URL from message
     url_regex = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
     urls = re.findall(url_regex, message_body)
